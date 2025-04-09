61473c663ac4147bd74f7902bf81ec889bd43383
travisluuly <131628691+travisluuly@users.noreply.github.com>
2025-04-08 20:42:37 -0400

    Update interface to prompt user for input

--- nserc_scraper.py
+++ nserc_scraper.py
@@ -1,17 +1,39 @@
-import requests
 import pandas as pd
-import json
-import time
-from datetime import datetime
 
-fields = ['Name-Nom', 'AreaOfApplicationCode']
+def exportNames(code):
+    fields = ['Name-Nom', 'AreaOfApplicationCode']
 
-df = pd.read_csv('2023.csv', encoding='latin-1', usecols=fields)
+    df = pd.read_csv('database.csv', encoding='latin-1', usecols=fields)
 
-filterBySubject = df[(df['AreaOfApplicationCode'] == 800)] # 800 is the information systems code
+    filterBySubject = df[(df['AreaOfApplicationCode'] == code)] # 800 is the information systems code
 
-df_Out = filterBySubject[['Name-Nom']]
+    df_Out = filterBySubject[['Name-Nom']]
 
-print (df_Out) #only returns names
+    print (df_Out) #only returns names
 
-df_Out.to_csv('names.csv')
+    df_Out.to_csv('names.csv')
+
+print("Welcome to NSERC DB Scraper")
+print("=================================")
+
+print("By default, this program scrapes the database and return names of award winners")
+
+print("")
+
+code = input("Please enter the area of application code for awards you would like to retrieve. Press enter to select default code. (Information systems is 800): ")
+if code == '':
+    code = 800
+else:
+    code = int(code)
+
+print("")
+
+year = input("Enter the range (in fiscal years) of databases that you want to pull from: ")
+
+print("")
+
+print("Showing the first and last 5 entries:")
+
+exportNames(code)
+
+print("names.csv has been exported.")
